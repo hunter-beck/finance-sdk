e1565b8676426aed31449830bd01485f8ff9b6cb
Hunter Beck <hunter.beck@cognite.com>
2020-08-13 17:30:29 +0200

    modified package to find_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,6 +13,6 @@ setup(
     author='Hunter Beck',
     author_email='hunterbeck3@gmail.com',
     license='unlicense',
-    packages=['finance'],
+    packages=find_packages('finance'),
     zip_safe=False
 )
