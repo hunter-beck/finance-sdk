1133e1d3ad92eec724880c063e7e312bbae12bc6
Hunter Beck <hunter.beck@cognite.com>
2020-08-13 16:53:52 +0200

    initial commit of setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='finance-sdk',
+    version='v0.0.0',
+    description='utils for building finance tools in python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='git@github.com:hunter-beck/finance-sdk.git',
+    author='Hunter Beck',
+    author_email='hunterbeck3@gmail.com',
+    license='unlicense',
+    packages=['finance'],
+    zip_safe=False
+)
